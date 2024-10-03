a11bfcbca316badd5db867bb5ae1bd0ce57d03d2
Gishnu Devadathan <jishnudevadathan1998@gmail.com>
2024-10-03 15:17:33 +0530

    activities 03-09-2024

---
+++ javascript/function/03-09-2024/pgm2.js
@@ -0,0 +1,19 @@
+// type coversion
+console.log("20"+5); //205
+console.log("20"-5); //15
+console.log("20"*5); //100
+console.log("20"/5); //4
+
+console.log(true+1); //2
+console.log(Number("42")); //42
+console.log(Number("hello")); //NaN
+
+console.log(typeof String(123)); //"123"
+console.log(String(true)); //true-string
+
+console.log(Boolean(0));//false
+console.log(Boolean(''));//false
+console.log(Boolean("hello"))//true
+
+console.log(parseInt("15.99"))//15
+console.log(parseFloat("3.14"))//3.14
